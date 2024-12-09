a440a3ca697b86173434e17659221024e71a6b76
Oseyijotham <jothamoboh16@gmail.com>
2024-12-09 19:38:51 +0100

    Update App

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="Yello-Connect">
+      <BrowserRouter basename="oasis-tasks">
         <App />
       </BrowserRouter>
     </Provider>
